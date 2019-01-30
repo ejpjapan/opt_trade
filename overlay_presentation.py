40780e76953c5ebe17a89970e45ddc252faac986
esbMac <bellord@berkeley.edu>
2019-01-30 07:09:23 -0500

    - Generalize hfr function to get hfrx, hfrr and hfri

--- overlay_presentation.py
+++ overlay_presentation.py
@@ -31,6 +31,8 @@ def main():
     heat_map_path = fig_path / 'heat_map.png'
     cum_perf_path = fig_path / 'cum_perf.png'
     cum_total_perf_path = fig_path / 'cum_total_perf.png'
+    cum_total_perf2_path = fig_path / 'cum_total_perf2.png'
+    hfri_overlay_path = fig_path / 'hfri_overlay.png'
     delta_path = fig_path / 'Delta.png'
     gamma_path = fig_path / 'Gamma.png'
     vega_path = fig_path / 'Vega.png'
@@ -46,7 +48,7 @@ def main():
     # Title slide
     for shape in prs.slides[0].placeholders:
         print('%d %s' % (shape.placeholder_format.idx, shape.name))
-    prs.slides[0].shapes[0].text = 'Generating Income with Index Options'
+    prs.slides[0].shapes[0].text = 'Income Alternatives'
 
     # 1
     slide = prs.slides.add_slide(prs.slide_layouts[layout_dict['TITLE_COLUMN1']])
@@ -79,7 +81,12 @@ def main():
     title_only_slide(cum_perf_path, layout_dict, prs, text_msg='Cumulative Excess Return')
     # 4
     title_only_slide(cum_total_perf_path, layout_dict, prs, text_msg='Cumulative Total Return')
-    # 5~9
+    # 5
+    title_only_slide(cum_total_perf2_path, layout_dict, prs, text_msg='Cumulative Total Return')
+    # 6
+    title_only_slide(hfri_overlay_path, layout_dict, prs, text_msg='Overlay vs. HFRI')
+
+    # 6~10
     greek_dict = {delta_path: 'Delta',
                   gamma_path: 'Gamma',
                   vega_path: 'Vega',
@@ -177,27 +184,30 @@ def get_fund_assets(update_funds=True):
             'QOPIX': 'Oppenheimer Fundamental Alternatives',# Fd $1.20
             'GAFYX': 'Natixis ASG Global Alternatives'} #  Fd $1.39bn
 
-        all_funds = [web.get_data_yahoo(key, 'JAN-16-04') for key, _ in fund_dict.items()]
+        all_funds = [web.get_data_yahoo(key, 'JAN-16-80') for key, _ in fund_dict.items()]
         all_funds = [fund['Adj Close'] for fund in all_funds]
         all_funds = [fund.rename(fund_name) for fund, fund_name in zip(all_funds, fund_dict.values())]
         all_funds = pd.concat(all_funds, axis=1)
+        # Replace dodgy observation
+        all_funds['Principal Global Multi-Strategy']['2017-08-24'] = all_funds['Principal Global Multi-Strategy'][
+            '2017-08-23']
         write_feather(all_funds, db_directory / feather_name)
 
     all_funds = read_feather(db_directory / feather_name)
     return all_funds
 
 
-def daily_hfrx(update_funds=True):
+def get_hfr(feather_name, csv_file_path, update_funds=True):
     db_directory = UpdateSP500Data.DATA_BASE_PATH / 'xl'
-    feather_name = 'hfrx.feather'
+    # feather_name = 'hfrx.feather'
     if update_funds:
         rows_to_skip = list(range(0, 2))
         headers = ['Date', 'Index Name', 'Index Code', 'Return', 'Index Value']
 
-        df = pd.read_csv(db_directory / 'hfrx_daily_index_data.csv', skiprows=rows_to_skip,
+        df = pd.read_csv(db_directory / csv_file_path, skiprows=rows_to_skip,
                          squeeze=True, names=headers, engine='python')
         index_codes = df['Index Code'].unique()
-        all_hfrx_list = []
+        all_hfr_list = []
         for index_code in index_codes[:-1]: # remove HFR company info
             idx = df['Index Code'] == index_code
             hfr = df[idx]
@@ -205,9 +215,54 @@ def daily_hfrx(update_funds=True):
             hfr = hfr.set_index(['Date'])
             hfr = hfr.reindex(hfr.index.sort_values())
             hfr_index = hfr['Index Value'].rename(hfr['Index Name'].unique()[0])
-            all_hfrx_list.append(hfr_index)
-        hfrx = pd.concat(all_hfrx_list, axis=1)
-        write_feather(hfrx, UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
+            all_hfr_list.append(hfr_index)
+        hfr_df = pd.concat(all_hfr_list, axis=1)
+        write_feather(hfr_df, UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
 
-    hfrx = read_feather(UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
-    return hfrx
+    hfr_df = read_feather(UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
+    return hfr_df
+
+
+def daily_hfrx(**kwargs):
+    feather_name = 'hfrx.feather'
+    csv_file_path = 'hfrx_daily_index_data.csv'
+    return get_hfr(feather_name, csv_file_path, **kwargs)
+
+
+def monthly_hfri(**kwargs):
+    feather_name = 'hfri.feather'
+    csv_file_path = 'hfri_index_data.csv'
+    return get_hfr(feather_name, csv_file_path, **kwargs)
+
+
+def monthly_hfrr(**kwargs):
+    feather_name = 'hfrr.feather'
+    csv_file_path = 'hfrr_index_data.csv'
+    return get_hfr(feather_name, csv_file_path, **kwargs)
+
+# def daily_hfrx(update_funds=True):
+#     db_directory = UpdateSP500Data.DATA_BASE_PATH / 'xl'
+#     feather_name = 'hfrx.feather'
+#     if update_funds:
+#         rows_to_skip = list(range(0, 2))
+#         headers = ['Date', 'Index Name', 'Index Code', 'Return', 'Index Value']
+#
+#         df = pd.read_csv(db_directory / 'hfrx_daily_index_data.csv', skiprows=rows_to_skip,
+#                          squeeze=True, names=headers, engine='python')
+#         index_codes = df['Index Code'].unique()
+#         all_hfrx_list = []
+#         for index_code in index_codes[:-1]: # remove HFR company info
+#             idx = df['Index Code'] == index_code
+#             hfr = df[idx]
+#             hfr.loc[:, 'Date'] = pd.to_datetime(hfr.loc[:, 'Date'])
+#             hfr = hfr.set_index(['Date'])
+#             hfr = hfr.reindex(hfr.index.sort_values())
+#             hfr_index = hfr['Index Value'].rename(hfr['Index Name'].unique()[0])
+#             all_hfrx_list.append(hfr_index)
+#         hfrx = pd.concat(all_hfrx_list, axis=1)
+#         write_feather(hfrx, UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
+#
+#     hfrx = read_feather(UpdateSP500Data.DATA_BASE_PATH / 'feather' / feather_name)
+#     return hfrx
+
+
